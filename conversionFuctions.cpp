55feb7a42bd5fbc496820513c5bbb6d0209501a8
subhashjogi15 <subhashjogi9184@gmail.com>
2024-09-26 01:05:46 +0530

    Update conversionFuctions.cpp

--- conversionFuctions.cpp
+++ conversionFuctions.cpp
@@ -3,13 +3,23 @@
 namespace TelCoColorCoder 
 {
     ColorPair GetColorFromPairNumber(int pairNumber) {
-	int zeroBasedPairNumber = pairNumber - 1;
-	MajorColor majorColor = static_cast<MajorColor>(zeroBasedPairNumber / numberOfMinorColors);
-	MinorColor minorColor = static_cast<MinorColor>(zeroBasedPairNumber % numberOfMinorColors);
-	return ColorPair(majorColor, minorColor);
+		int zeroBasedPairNumber = pairNumber - 1;
+		MajorColor majorColor = static_cast<MajorColor>(zeroBasedPairNumber / numberOfMinorColors);
+		MinorColor minorColor = static_cast<MinorColor>(zeroBasedPairNumber % numberOfMinorColors);
+		return ColorPair(majorColor, minorColor);
     }
     
 	int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
 		return major * numberOfMinorColors + minor + 1;
+	}
+
+	void printColorCodingManual() {
+		int pairNumber = 1;
+    	for (int majorColorIterator = 0; majorColorIterator < 5; ++majorColorIterator) {
+        for (int minorColorIterator = 0; minorColorIterator < 5; ++minorColorIterator) {
+            std::cout << "Pair Number " << pairNumber << ": " << TelCoColorCoder::MajorColorNames[majorColorIterator] << " - " << TelCoColorCoder::MinorColorNames[minorColorIterator] << std::endl;
+            ++pairNumber;
+        }
+    }
 	}
 }
