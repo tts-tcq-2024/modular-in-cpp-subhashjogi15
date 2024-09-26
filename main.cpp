99599baa60b25457352f8383de3f49fc21cb557f
subhashjogi15 <subhashjogi9184@gmail.com>
2024-09-26 12:01:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
-//#include "TelCoColorCoder.h"
-#include "testFunctions.cpp"
+#include "TelCoColorCoder.h"
+//#include "testFunctions.cpp"
+
+void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor)
+{
+    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair: " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber)
+{
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number: " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
 
 int main() {
-    testNumberToPair(4, TelCoColorCoder::MajorColor WHITE, TelCoColorCoder::MinorColor BROWN);
-    testNumberToPair(5, TelCoColorCoder::MajorColor WHITE, TelCoColorCoder::MinorColor SLATE);
+    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
-    testPairToNumber(TelCoColorCoder::MajorColor BLACK, TelCoColorCoder::MinorColor ORANGE, 12);
-    testPairToNumber(TelCoColorCoder::MajorColor VIOLET, TelCoColorCoder::MinorColor SLATE, 25);
+    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
     return 0;
 }
