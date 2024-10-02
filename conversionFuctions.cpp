67d73a88f16c0120f21eef6c708269924a7f147e
subhashjogi15 <subhashjogi9184@gmail.com>
2024-10-03 01:45:04 +0530

    Update conversionFuctions.cpp

--- conversionFuctions.cpp
+++ conversionFuctions.cpp
@@ -15,15 +15,11 @@ namespace TelCoColorCoder
 
 	std::list<numberWithPair> generateColorCodingManual() {
 		int pairNumber = 1;
-		int iterator = 0;
 		std::list<numberWithPair> generatedManual;
     	for (int majorColorIterator = 0; majorColorIterator < 5; ++majorColorIterator) {
 	        for (int minorColorIterator = 0; minorColorIterator < 5; ++minorColorIterator) {
-				generatedManual[iterator].pairNumber = pairNumber;
-				generatedManual[iterator].majorColor = TelCoColorCoder::MajorColorNames[majorColorIterator];
-				generatedManual[iterator].minorColor = TelCoColorCoder::MinorColorNames[minorColorIterator];
-				iterator++;
-				pairNumber++;
+			generatedManual.push_back({pairNumber, TelCoColorCoder::MajorColorNames[majorColorIterator], TelCoColorCoder::MinorColorNames[minorColorIterator]});
+			pairNumber++;
 	        }
     	}
 		return generatedManual;
