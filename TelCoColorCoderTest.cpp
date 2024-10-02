4bf631651a25e4c06cd62592ac0f7360a0f4d69a
subhashjogi15 <subhashjogi9184@gmail.com>
2024-10-03 01:37:26 +0530

    Update TelCoColorCoderTest.cpp

--- TelCoColorCoderTest.cpp
+++ TelCoColorCoderTest.cpp
@@ -23,20 +23,12 @@ void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorC
     assert(pairNumber == expectedPairNumber);
 }
 
-void testgenerateColorCodingManual() {
-    std::list<numberWithPair> numberWithPairManual;
-    expectedNumberWithPairManual = generateColorCodingManual();
-    EXPECT_EQ(numberWithPairManual, expectedNumberWithPairManual);
-}
-
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-
-    testgenerateColorCodingManual();
 
     return 0;
 }
